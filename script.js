6c3da1a8739b49f188c1a0626515dca343b5d420
Izuchukwu Abugu <137216438+ARMANIE4L@users.noreply.github.com>
2023-10-20 00:37:40 +0100

    new changes to script

--- script.js
+++ script.js
@@ -1,61 +1,86 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    const quotes = [
-        'Armanie thinks you are the most beautiful girl ever',
-        'Armanie loves you so much',
-        'You are the "JOY" of his life',
-        "Please don't break Armanie's heart",
-        'Let us be the longest lasting couple ever, I do not want to ever break up even in the coming years',
-        'Let us be there for each other always',
-        'Be open to me always, do not lie about your feelings',
-        'Armanie appreciates you sticking with him at all times, not going unnoticed',
-        'Armanie gets butterflies in his belly whenever you say you love him',
-        'Armanie wants you to be happy always',
-        "You are the light that brightens up Armanie's day",
-        'Armanie cherishes every moment spent with you',
-        "Your smile melts Armanie's heart every time",
-        'Armanie is grateful for your love and support',
-        'You make Armanie believe in true love',
-        'Your presence makes every day more beautiful for Armanie',
-        'Armanie is the luckiest person to have you in his life',
-        "You are the missing piece of Armanie's heart",
-        'Armanie loves you more than words can express',
-        'Your love is the reason Armanie wakes up with a smile',
-        'You are the reason Armanie believes in happily ever after',
-        "Armanie's love for you grows stronger every day",
-        'Every moment with you is a treasure for Armanie',
-        'Armanie is forever grateful for your love and support',
-        "You are the one who makes Armanie's world complete",
-        "Armanie's heart beats only for you",
-        "Your love fills Armanie's life with joy and happiness",
-        "You are the epitome of beauty and grace in Armanie's eyes",
-        'Armanie is blessed to have you as his partner in life',
-        "Your love has transformed Armanie's life in the most beautiful ways",
-      ];
-
-// function getRandomIndex(quotesLength) {
-//     return Math.floor(Math.random() * quotes.length)    
-// }
 
 let currentIndex = 0;
 
-function displayQuote() {
+function displayQuote(event) {
 
- let nameInput = document.querySelector('#nameInput').value
  let quoteSpace = document.querySelector('#quote')
+ let nameInput = document.querySelector('#nameInput').value
 
-  quoteSpace.textContent =  nameInput + ',' + ' ' + quotes[currentIndex]
+ // LEARN AJAX FROM HERE
+ const xhr = new XMLHttpRequest();
+ xhr.open('GET', './quotes.json', true);
+ xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+            const quotes = JSON.parse(xhr.responseText);
 
-  quoteSpace.classList.add('show')
-  
-  currentIndex++
-  if (currentIndex >= quotes.length) {
-     quoteSpace.innerHTML = "That's all for now " + nameInput + ',' + ' welcome back!' + '<br>' + 'With Love, ARMANIE'
+            if (currentIndex < quotes.length) {
+            let message = nameInput + ',' + ' ' + quotes[currentIndex]
+
+            quoteSpace.textContent = message 
+           
+            quoteSpace.classList.add('show')
+            currentIndex++                
+
+            } else {
+                quoteSpace.innerHTML = "That's all for now " + nameInput + ',' + ' welcome back!' + '<br>' + 'With Love, ARMANIE'
+
+            }
+
+        } else {
+            console.log('Error:', xhr.status)
+            quoteSpace.textContent = 'Failed to fetch quote.'
+            quoteSpace.classList.add('show')
+        }
+    }
  }
  
 
+ // make an ajax GET request to fetch thr JSON file
+
+
+ xhr.send()
+ 
+ }
+ 
+ // trying to change the button text after being clicked once
+function updateButtonText(event) {
+    let generateButton = document.querySelector('#generateButton')
+    let nameInput = document.querySelector('#nameInput').value
+    generateButton.textContent = 'Click Again ' + nameInput + '!'
 }
 
-document.querySelector('#generateButton').addEventListener('click', displayQuote )
+function lastButtonClick(event) {
+        let generateButton = document.querySelector('#generateButton')
+        let nameInput = document.querySelector('#nameInput').value
+        generateButton.textContent = 'Final Click ' + nameInput + '!'
+}
+
+document.querySelector('#generateButton').addEventListener('click', function generateButtonClickHandler() {
+    displayQuote();
+    if (currentIndex >= 0) {
+        updateButtonText()
+    }
+    if (currentIndex === 29) {
+        lastButtonClick()
+    }
+    if (currentIndex === 30) {
+        lastButtonClick()
+    }
+}
+)
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        displayQuote()
+        if (currentIndex === 0) {
+           updateButtonText() 
+        }
+    }
+}
+
+document.querySelector('#nameInput').addEventListener('keydown', handleKeyPress)
 
 })
